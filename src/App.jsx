e2c7c9d13886e7114a16e9f2b11ea118759f9850
React-Coder0912 <mothe.shivaraj09@gmail.com>
2026-04-17 10:58:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-lime-400">
+        Tailwind is working ✅
+      </h1>
+      <h2 className="">Tailwind</h2>
+    </div>
+  )
+}
+
+export default App
